84fc2a2b44d6dbf03f6ebc5bb3291d337a8e4c6b
yassine <abidiyassine@outlook.com>
2025-03-04 20:04:23 +0100

    auth crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
@@ -15,6 +16,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       synchronize: true,
       logging:true,
     }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
